d78984fb869e397a5f6ec3f0e0043276346d7450
Maksym Sereda <maksym.se@gmail.com>
2021-09-22 14:43:28 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,54 @@
+//  Преобразование числа в массив цифр
+function convertNumberToArray(number) {
+  const arr = [];
+  let code = number;
+
+  while (code > 0) {
+    arr.push(code % 10);
+
+    code = parseInt(code / 10);
+  }
+  arr.reverse();
+
+  return arr;
+}
+
+// Функция вычисления бонусов
+const calculationOfBonuses = (promoCode) => {
+  const arrayOfDigits = convertNumberToArray(promoCode);
+  let pairsOfOddDigits = [];
+  let sumOfEvenDigits = 0;
+  let sumOfOddDigits = 0;
+
+  let pair = [];
+
+  arrayOfDigits.forEach((digit) => {
+    if (digit % 2 !== 0) {
+      pair.push(digit);
+      sumOfOddDigits += digit;
+
+      if (pair.length === 2) {
+        pairsOfOddDigits.push(pair);
+      }
+    } else if (digit % 2 === 0) {
+      sumOfEvenDigits += digit;
+      pair = [];
+    }
+  });
+
+  if (pairsOfOddDigits.length < 2) {
+    if (sumOfEvenDigits > sumOfOddDigits) {
+      return 100;
+    } else {
+      return 0;
+    }
+  } else {
+    const arr = pairsOfOddDigits.flat();
+
+    if (arr[0] < arr[1] && arr[arr.length - 2] < arr[arr.length - 1]) {
+      return 2000;
+    } else {
+      return 1000;
+    }
+  }
+};
